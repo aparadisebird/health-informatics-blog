d4ba0923a09204c0c751ef1766a6e594d0ecfb7a
Shifat Shahriar Siam <siam.bd.1970@gmail.com>
2026-01-03 08:37:41 +0000

    Setup environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjvTISsUZsWmtKPVwRGVHiM_oUedU_BLs",
-  authDomain: "the-health-nexus.firebaseapp.com",
-  projectId: "the-health-nexus",
-  storageBucket: "the-health-nexus.firebasestorage.app",
-  messagingSenderId: "907886498214",
-  appId: "1:907886498214:web:601c880bbeeecba51dff67"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
